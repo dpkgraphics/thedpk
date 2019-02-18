9f89908aeb90b14c4d7aa7d445eb6a25385e9fc0
Daniel <daniel.kinsella@insuresign.com>
2019-02-18 10:10:00 -0500

    Updates

--- precache-manifest.412f57f14a1bbfb7cb2595b50f07975c.js
+++ precache-manifest.412f57f14a1bbfb7cb2595b50f07975c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c5058e3e31214a448fce",
+    "revision": "25721d017ccee41f04cd",
     "url": "/thedpk/static/css/main.17e41cf3.chunk.css"
   },
   {
-    "revision": "c5058e3e31214a448fce",
-    "url": "/thedpk/static/js/main.c5058e3e.chunk.js"
+    "revision": "25721d017ccee41f04cd",
+    "url": "/thedpk/static/js/main.25721d01.chunk.js"
   },
   {
     "revision": "0f05c13dcdc195741d2e",
@@ -96,12 +96,12 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/dpk-header-repeat.b166003f.jpg"
   },
   {
-    "revision": "987b84570ea69ee660455b8d5e91f5f1",
-    "url": "/thedpk/static/media/roboto-latin-100.987b8457.woff2"
-  },
-  {
     "revision": "55536c8e9e9a532651e3cf374f290ea3",
     "url": "/thedpk/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/thedpk/static/media/roboto-latin-100.987b8457.woff2"
   },
   {
     "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
@@ -120,16 +120,20 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/roboto-latin-900.19b7a0ad.woff2"
   },
   {
-    "revision": "d704bb3d579b7d5e40880c75705c8a71",
-    "url": "/thedpk/static/media/roboto-latin-100italic.d704bb3d.woff"
-  },
-  {
     "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
     "url": "/thedpk/static/media/roboto-latin-100.e9dbbe8a.woff"
   },
   {
     "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
     "url": "/thedpk/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/thedpk/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/thedpk/static/media/roboto-latin-100italic.d704bb3d.woff"
   },
   {
     "revision": "a1471d1d6431c893582a5f6a250db3f9",
@@ -140,20 +144,12 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/roboto-latin-300italic.210a7c78.woff"
   },
   {
-    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
-    "url": "/thedpk/static/media/roboto-latin-300italic.d69924b9.woff2"
-  },
-  {
-    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
-    "url": "/thedpk/static/media/roboto-latin-400.bafb105b.woff"
-  },
-  {
     "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
     "url": "/thedpk/static/media/roboto-latin-400italic.9680d5a0.woff"
   },
   {
-    "revision": "de8b7431b74642e830af4d4f4b513ec9",
-    "url": "/thedpk/static/media/roboto-latin-500.de8b7431.woff"
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/thedpk/static/media/roboto-latin-400.bafb105b.woff"
   },
   {
     "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
@@ -168,6 +164,10 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/roboto-latin-500italic.510dec37.woff2"
   },
   {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/thedpk/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
     "revision": "010c1aeee3c6d1cbb1d5761d80353823",
     "url": "/thedpk/static/media/roboto-latin-700italic.010c1aee.woff2"
   },
@@ -180,6 +180,10 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/roboto-latin-700.cf6613d1.woff"
   },
   {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/thedpk/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
     "revision": "7b770d6c53423deb1a8e49d3c9175184",
     "url": "/thedpk/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
@@ -188,11 +192,7 @@ self.__precacheManifest = [
     "url": "/thedpk/static/media/roboto-latin-900italic.bc833e72.woff"
   },
   {
-    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
-    "url": "/thedpk/static/media/roboto-latin-900.8c2ade50.woff"
-  },
-  {
-    "revision": "f5fccd9c1944e81260ad985c183c5fdd",
+    "revision": "2ab54fec5f09e510a8a39f5aebcbd95a",
     "url": "/thedpk/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/thedpk/precache-manifest.3ef7d8a5406777efc5260ca6a1ad584a.js"
+  "/thedpk/precache-manifest.412f57f14a1bbfb7cb2595b50f07975c.js"
 );
 
 workbox.clientsClaim();
